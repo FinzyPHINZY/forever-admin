16fbfa69732e7ac0c7ea9b1afd3d571b2a4591d6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-01-28 11:19:05 +0100

    feat: implemented authentication system

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,40 @@ import { Route, Routes } from 'react-router-dom';
 import Add from './pages/Add';
 import List from './pages/List';
 import Orders from './pages/Orders';
+import { useState, useEffect } from 'react';
+import Login from './components/Login';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem('token') ? localStorage.getItem('token') : ''
+  );
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr />
-        <div className="flex w-full">
-          <Sidebar />
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-            <Routes>
-              <Route path="/add" element={<Add />} />
-              <Route path="/list" element={<List />} />
-              <Route path="/orders" element={<Orders />} />
-            </Routes>
+      <ToastContainer />
+      {token === '' ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
